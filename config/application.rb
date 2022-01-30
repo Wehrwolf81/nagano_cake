313c311c7b8ee1b367bafbfe46ae34c79dbda0f5
Wehrwolf81 <sistine1224@gmail.com>
2022-01-30 16:47:17 +0000

    [Update]

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
+
 module NaganoCake
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -13,3 +15,4 @@ module NaganoCake
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
