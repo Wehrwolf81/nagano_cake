351e30dd064c4d21ac50d955713ff1a810ec33cc
Wehrwolf81 <sistine1224@gmail.com>
2022-02-18 16:09:23 +0000

    [update]

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,10 +1,12 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order=Order.find(params[:id])
+    @cart_items=current_customer.cart_items.all
   end
-  
+
   def update_order_status
   end
-  
+
   def update_making_status
-  end  
+  end
 end
