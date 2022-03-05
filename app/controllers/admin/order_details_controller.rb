9e3b48c8e3947fdef9096b8aa0157084cd02a711
Wehrwolf81 <sistine1224@gmail.com>
2022-03-05 08:54:20 +0000

    [update]

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,9 +1,9 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
-    @order_detail = Order.find(params[:id])
+    @order_detail = OrderDetail.find(params[:id])
     if @order_detail.update(order_detail_params)
-      redirect_to admin_order_path(@order.id)
+      redirect_to admin_order_path(@order_detail.order_id)
     else
       render :show
     end
