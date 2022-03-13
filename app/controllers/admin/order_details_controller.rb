f676da440945cd67461ba3fc877e2ef9f25d0b93
Wehrwolf81 <sistine1224@gmail.com>
2022-03-13 05:21:52 +0000

    [update]

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,34 +1,36 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
-    # binding.pry
+
 
      @order_detail = OrderDetail.find(params[:id])
+    # binding.pry
+    # @order=Order.find(params[:id])
+
     if params[:order_detail][:making_status]=="making"
       @order_detail.making_status = params[:order_detail][:making_status]
       # binding.pry
       @order_detail.update(order_detail_params)
-      @order=Order.find(params[:id])
-
-
-        @order.status = "making"
-        @order.update(status:"making")
-      redirect_to admin_order_path(@order.id)
+        @order_detail.order.status = "making"
+        @order_detail.order.update(status:"making")
+      redirect_to admin_order_path(@order_detail.order)
     elsif params[:order_detail][:making_status]=="complete"
-      @order_detail.status = params[:order_detail][:making_status]
+      @order_detail.making_status = params[:order_detail][:making_status]
       @order_detail.update(order_detail_params)
-         @order=Order.find(params[:id])
-      if @order.order_details.count == @order.order_details.where(making_status: "発送準備中").count
-        order.status = "ready_to_ship"
-        order.update(status:"ready_to_ship")
+      if @order_detail.order.order_details.count == @order_detail.order.order_details.where(making_status: "complete").count
+        @order_detail.order.status = "ready_to_ship"
+        @order_detail.order.update(status:"ready_to_ship")
       end
-      redirect_to admin_order_path(@order.id)
+      redirect_to admin_order_path(@order_detail.order)
     end
   end
 
 private
   def order_detail_params
     params.require(:order_detail).permit(:making_status)
+  end
+  def order_params
+    params.require(:order).permit(:order_id, :status)
   end
 
 end
